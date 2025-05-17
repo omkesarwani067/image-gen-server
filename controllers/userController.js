405aae764c421628e41a58976ab3696c1b2d0484
omkesarwani067 <omkesarwani067@gmail.com>
2025-05-17 19:12:34 +0530

    first commit

---
+++ controllers/userController.js
@@ -0,0 +1,86 @@
+
+import userModel from "../models/userModels.js";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.json({ success: false, message: 'Missing Details' })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const userData = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        const newUser = new userModel(userData)
+        const user = await newUser.save()
+
+        const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET)
+
+        res.json({ success: true, token, user: { name: user.name, creditBalance: user.creditBalance } });
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email })
+
+        if (!user) {
+            return res.json({ success: false, message: 'User does not exist' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (isMatch) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+
+            res.json({ success: true, token, user: { name: user.name, creditBalance: user.creditBalance } });
+
+        } else {
+            return res.json({ success: false, message: 'Invalid credentials' })
+        }
+
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+const userCredits = async (req, res) => {
+    try {
+        const token = req.headers.token;
+        if (!token) return res.json({ success: false, message: 'No token provided' });
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded.id);
+        if (!user) return res.json({ success: false, message: 'User not found' });
+
+        res.json({
+            success: true,
+            credits: user.creditBalance,
+            user: { name: user.name, creditBalance: user.creditBalance }
+        });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+
+export  {registerUser, loginUser, userCredits}
+
